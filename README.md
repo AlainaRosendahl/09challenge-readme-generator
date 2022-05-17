f3eb4b978e11412649f70f4424f5ca0ca47d6e72
Alaina Rosendahl <elenarosendahl16@gmail.com>
2022-05-17 11:22:23 -0500

    readme.md

--- README.md
+++ README.md
@@ -1,42 +1,25 @@
-# Professional ReadMe-Generator
-## Description
-Created to make a re-useable and consistent read-me for ALL projects! Please enjoy using it on your projects as well.
-![badge](https://img.shields.io/badge/license-undefined-brightorange)
-You can access more badges and their purposes at [shields.io](https://shields.io)
-## Table of Contents
-  * [Installation](#installation)
-  * [Contribution](#contribution)
-  * [Testing](#testing)
-  * [Questions](#questions)
-  * [License](#license)
-    
-    
-## Installation
-    
-  _Follow these steps to properly install this application:_
-  Download/Clone from Github and Install Inquirer.
-      
-## Contribution
-  _If you would like to contribute, please adhere to these guidelines:_
-  Please do not contribute
-      
-## Testing
-  _Instructions for testing application:_
-  I will provide a video of working application
-      
-## Questions
-      
-  _For further questions:_
-  undefined
-  
-  _Contact Info:_
-  GitHub: [undefined](https://github.com/undefined)
-  Email: [elenarosendahl16@gmail.com](mailto:elenarosendahl16@gmail.com)
-    
-## License
-      
-  _This application has the undefined._
-      
-  For more information please view the [license description]().
-  
-  
+# Professional read-me generator
+  ## Licensing:
+  [![license](https://img.shields.io/badge/license-MIT-blue)]Find info (https://shields.io)
+  ## Table of Contents 
+  - [Description](#description)
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [Contribution](#contribution)
+  - [Testing](#testing)
+  - [Additional Info](#additional-info)
+  ## Description:
+  This application was created to generate a high-quality, professional README depending on a series of questions a user answers in their command line using Node.js
+  ## Installation:
+  Download/Clone from Github, Install inquirer with the repository open in terminal.
+  ## Usage:
+  Open terminal, Type "node index.js" in the command line, Answer the following prompts, If application was successful a README.d will be created.
+  ## License:
+  MIT
+  ## Contribution:
+  Please dont.
+  ## Testing:
+  I will provide viedo of the working application.
+  ## Additional Info:
+  - Github: [AlainaRosendahl](https://github.com/AlainaRosendahl)
+  - Email: elenarosendahl16@gmail.com 
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,129 +1,30 @@
+// function to generate markdown for README
 function generateMarkdown(data) {
-    let markdownTemplate =
-    `# ${data.title}
- 
-    ## Description 
-    ${data.description} 
-    `;
-
-    let tableOfContents = `## **Table of Contents**
-    ---`;
-    if (data.installation) {
-      tableOfContents += `* [Installation](#installation)`
-    };
-    if (data.instructions) {
-      tableOfContents += `* [Usage](#usage)`
-    };
-    if (data.contribution) {
-      tableOfContents += `* [Contribution](#contribution)`
-    };
-    if (data.testing) {
-      tableOfContents += `* [Testing](#testing)`
-    };
-    if (data.questions) {
-      tableOfContents += `* [Questions](#questions)`
-    };
-  
- 
-    markdownTemplate += tableOfContents;
-    
- 
-    markdownTemplate +=
-      `* [Contact](#contact)`;
-    markdownTemplate +=
-      `* [License](#license)
-      ---`;
-    
-
-    
-    //installation
-    if (data.installation) {
-      markdownTemplate +=
-        `## Installation
-        ---
-        _Follow these steps to properly install this application:_
-        ${data.installation}`
-    };
-    
-    //usage
-    if (data.instructions) {
-      markdownTemplate +=
-        `## Usage
-        ---
-        _Instructions for use:_
-        ${data.instructions}`
-    };
-  
-    //contributions
-    if (data.contribution) {
-      markdownTemplate +=
-        `## Contribution
-        ---
-        _If you would like to contribute, please adhere to these guidelines:_
-        ${data.contribution}`
-    };
-  
-    //testing
-    if (data.testing) {
-      markdownTemplate +=
-        `## Testing
-        ---
-        _Instructions for testing application:_
-        ${data.testing}`
-    };
-  
-    //questions
-    if (data.questions) {
-      markdownTemplate +=
-        `## Questions
-        ---
-        _For further questions please follow these steps:_
-        ${data.questions}`
-    }
-  
-    //contact
-    markdownTemplate +=
-      `## Contact
-        ---
-        _Contact Info:_
-        * GitHub: [${data.username}](https://github.com/${data.username})
-        * Email: [${data.email}](${data.email})`;
-    
-    //license
-    let licenseOption = `${data.license}`;
-    let licenseLink = '';
-    //if conditional to give the license the correct link
-    if (licenseOption === 'GNU AGPLv3') {
-      licenseLink = 'https://choosealicense.com/licenses/agpl-3.0/';
-    };
-    if (licenseOption === 'GNU GPLv3') {
-      licenseLink = 'https://choosealicense.com/licenses/gpl-3.0/';
-    };
-    if (licenseOption === 'GNU LGPLv3') {
-      licenseLink = 'https://choosealicense.com/licenses/lgpl-3.0/';
-    };
-    if (licenseOption === 'Mozilla Public License 2.0') {
-      licenseLink = 'https://choosealicense.com/licenses/mpl-2.0/';
-    };
-    if (licenseOption === 'Apache License 2.0') {
-      licenseLink = 'https://choosealicense.com/licenses/apache-2.0/';
-    };
-    if (licenseOption === 'MIT License') {
-      licenseLink = 'https://choosealicense.com/licenses/mit/';
-    };
-    if (licenseOption === 'Boost Software License 1.0') {
-      licenseLink = 'https://choosealicense.com/licenses/bsl-1.0/';
-    };
-    if (licenseOption === 'The Unlicense') {
-      licenseLink = 'https://choosealicense.com/licenses/unlicense/';
-    };
-  
-    markdownTemplate +=
-      `## License
-        ---
-        This application has the ${licenseOption}. 
-        For more information please view the [license description](${licenseLink}).`;
-    return markdownTemplate;
-  }
+  return `# ${data.title}
+  ## Licensing:
+  [![license](https://img.shields.io/badge/license-${data.licensing}-blue)]Find info (https://shields.io)
+  ## Table of Contents 
+  - [Description](#description)
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [Contribution](#contribution)
+  - [Testing](#testing)
+  - [Additional Info](#additional-info)
+  ## Description:
+  ${data.description}
+  ## Installation:
+  ${data.installation}
+  ## Usage:
+  ${data.usage}
+  ## License:
+  ${data.licensing}
+  ## Contribution:
+  ${data.contribution}
+  ## Testing:
+  ${data.testing}
+  ## Additional Info:
+  - Github: [${data.github}](https://github.com/${data.github})
+  - Email: ${data.email} `;
+}
 
 module.exports = generateMarkdown;
